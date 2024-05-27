c9971b2f179aac2ef306670b1ce4b381f2e41789
Afsl Thaj <afsal.taj06@gmail.com>
2024-05-27 16:47:59 +1000

    Add a type in the caller

--- caller/src/lib.rs
+++ caller/src/lib.rs
@@ -1,4 +1,4 @@
-use crate::bindings::exports::test::caller::api::Guest;
+use crate::bindings::exports::test::caller::api::{Guest, NewType};
 use crate::bindings::test::callee_stub::stub_callee::Api;
 use crate::bindings::golem::rpc::types::Uri;
 
@@ -8,15 +8,17 @@ mod bindings;
 struct Component;
 
 impl Guest for Component {
-    fn run_self(name: String) -> String {
+    fn run_self(name:  String) -> NewType {
         let component_id = "callee_component_id".to_string();
         let uri = Uri { value: format!("worker://{component_id}/{}", "foo") };
         let callee_api = Api::new(&uri);
         let result = callee_api.run_callee(name.as_str());
-        result
+        NewType{
+            value: result
+        }
     }
 
-    fn run_remote(remote: String) -> String {
-        format!("From remote {}", remote.to_string())
+    fn run_remote(remote: NewType) -> String {
+        format!("From remote {}", remote.value.to_string())
     }
 }
