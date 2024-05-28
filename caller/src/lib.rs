d1153490594141d027cd4ee8a55cb50c8232faea
Afsl Thaj <afsal.taj06@gmail.com>
2024-05-28 12:55:56 +1000

    Include only caller

--- caller/src/lib.rs
+++ caller/src/lib.rs
@@ -1,6 +1,4 @@
 use crate::bindings::exports::test::caller::api::{Guest, NewType};
-use crate::bindings::test::callee_stub::stub_callee::Api;
-use crate::bindings::golem::rpc::types::Uri;
 
 
 mod bindings;
@@ -8,17 +6,9 @@ mod bindings;
 struct Component;
 
 impl Guest for Component {
-    fn run_self(name:  String) -> NewType {
-        let component_id = "callee_component_id".to_string();
-        let uri = Uri { value: format!("worker://{component_id}/{}", "foo") };
-        let callee_api = Api::new(&uri);
-        let result = callee_api.run_callee(name.as_str());
-        NewType{
-            value: result
+    fn run(name: String) -> NewType {
+        NewType {
+            value: format!("Hello, {}!", name)
         }
-    }
-
-    fn run_remote(remote: NewType) -> String {
-        format!("From remote {}", remote.value.to_string())
     }
 }
