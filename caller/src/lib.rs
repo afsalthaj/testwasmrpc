aab94e2f5bf425144002dbec56c32be0429cf86e
Afsl Thaj <afsal.taj06@gmail.com>
2024-05-29 12:47:32 +1000

    Cyclic dependency direct complete

--- caller/src/lib.rs
+++ caller/src/lib.rs
@@ -1,14 +1,23 @@
 use crate::bindings::exports::test::caller::api::{Guest, NewType};
-
+use crate::bindings::golem::rpc::types::Uri;
+use crate::bindings::test::caller_stub::stub_caller::Api;
 
 mod bindings;
 
 struct Component;
 
 impl Guest for Component {
-    fn run(name: String) -> NewType {
+
+    fn run(component_id: String) -> NewType {
+        let uri = Uri { value: format!("worker://{component_id}/{}", "myworker") };
+
+        let api = Api::new(&uri);
+
+        // Cyclic call
+        let result = api.run("foo");
+
         NewType {
-            value: format!("Hello, {}!", name)
+            value: result.value
         }
     }
 }
