9cfade79d2333f9caf9044a2d072632f2cf71d37
Afsl Thaj <afsal.taj06@gmail.com>
2024-05-30 13:40:02 +1000

    x to y while y to z along with x to x

--- caller/src/lib.rs
+++ caller/src/lib.rs
@@ -1,7 +1,7 @@
 use crate::bindings::exports::test::caller::api::{Guest, NewType};
 use crate::bindings::test::callee_stub::stub_callee::{Api, Uri};
-// use crate::bindings::golem::rpc::types::Uri;
-// use crate::bindings::test::caller_stub::stub_caller::Api;
+use crate::bindings::test::caller_stub::stub_caller::Api as CyclicApi;
+use crate::bindings::test::caller_stub::stub_caller::Uri as CyclicUri; // Sort of same as above doesn't matter
 
 mod bindings;
 
@@ -9,6 +9,15 @@ struct Component;
 
 impl Guest for Component {
     fn add(x: i32, y: i32) -> i64 {
+        let uri = CyclicUri { value: format!("worker://callercomponentitself/{}", "myworker") };
+
+        let api = CyclicApi::new(&uri);
+
+        // Cyclic call
+        let result = api.run("foo");
+
+        dbg!(result.value);
+
         (x + y) as i64
     }
 
