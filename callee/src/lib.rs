32edbe26adeec77bf3e2427e4a94729bce61ea81
Afsl Thaj <afsal.taj06@gmail.com>
2024-06-09 18:48:59 +1000

    Remove debug lines

--- callee/src/lib.rs
+++ callee/src/lib.rs
@@ -1,13 +1,21 @@
 use crate::bindings::exports::test::callee::api::{CalleeType, Guest};
+use crate::bindings::test::caller_stub::stub_caller::{Api, Uri};
 
 mod bindings;
 
 struct Component;
 
 impl Guest for Component {
-    fn run(name: String) -> CalleeType {
+    fn run(caller_component_id: String) -> CalleeType {
+        let uri = Uri { value: format!("worker://{caller_component_id}/{}", "myworker") };
+
+        let api = Api::new(&uri);
+
+        // Cyclic call
+        let result = api.run("foo");
+
         CalleeType {
-            value: format!("Hello, {}!", name),
+            value: result.value
         }
     }
 }
